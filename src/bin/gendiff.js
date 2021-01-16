c7a6d907e16e074f2a17730d4f67a76fda129aaa
Alex Mitrofanov <alexloves.114@gmail.com>
2021-01-16 14:48:34 +0300

    add commander to gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log('Hi');
+import commander from 'commander';
+
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const packageConfig = require('../../package.json');
+
+const { program } = commander.program;
+
+program.version(`${packageConfig.version}`);
+
+program.description('Compares two configuration files and shows a difference');
+
+program.parse(process.argv);
