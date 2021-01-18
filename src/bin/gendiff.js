183e800b24fb81edb5ef44b41d51a7bbc0b40a68
Alex Mitrofanov <alexloves.114@gmail.com>
2021-01-18 18:30:11 +0300

    add packegeConfig

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -9,8 +9,10 @@ const packageConfig = require('../../package.json');
 
 const { program } = commander.program;
 
-program.version(`${packageConfig.version}`);
-
-program.description('Compares two configuration files and shows a difference');
+program
+  .version(`${packageConfig.version}`)
+  .description('Compares two configuration files and shows a difference')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> </filepath2>');
 
 program.parse(process.argv);
