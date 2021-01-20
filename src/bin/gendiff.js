89d0cc920dd880ca718bb530e873705c2ac075c8
Alex Mitrofanov <alexloves.114@gmail.com>
2021-01-20 19:59:49 +0300

    add action to program module gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -9,26 +9,32 @@ const require = createRequire(import.meta.url);
 const packageConfig = require('../../package.json');
 
 const { program } = commander.program;
+/*
+const fs = require('fs');
+const path = require('path');
+
+const path1 = path.resolve('file1.json');
+const path11 = process.cwd(path1);
+console.log(path11);
+console.log(path1);
+const filee1 = fs.readFileSync('/home/alex/frontend-project-lvl2/src/file1.json', 'utf8');
+console.log(filee1);
+*/
 
 program
   .version(`${packageConfig.version}`)
   .description('Compares two configuration files and shows a difference')
   .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> </filepath2>');
+  .arguments('<filepath1> </filepath2>')
+  .action((filepath1, filepath2) => {
+    // const fs = require('fs');
+    // const path = require('path');
+    // const path1 = process.cwd(filepath1);
+    // console.log(path.resolve(filepath1));
+    // const file1 = fs.readFileSinc(path.resolve(filepath1), 'utf8');
+    // const file2 = fs.readFileSinc('filepath2');
+    // console.log(file1);
+    getDifference(filepath1, filepath2);
+  });
 
 program.parse(process.argv);
-
-const arg1 = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false,
-};
-
-const arg2 = {
-  timeout: 20,
-  verbose: true,
-  host: 'hexlet.io',
-};
-
-getDifference(arg1, arg2);
--- src/getdifference.js
+++ src/getdifference.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 
 const getDifference = (file1, file2) => {
+  // const fil1 = fs.readFileSinc(file1);
+
   const keys1 = Object.keys(file1).sort();
   const keys2 = Object.keys(file2).sort();
   const keys = keys2.reduce((acc, key) => {
@@ -12,23 +14,23 @@ const getDifference = (file1, file2) => {
   const resultObject = keys.reduce((acc, key) => {
     if (_.has(file1, key) && _.has(file2, key)) {
       if (file2[key] === file1[key]) {
-        acc[`  ${key}`] = file2[key];
+        acc[`    ${key}`] = file2[key];
       }
       if (file2[key] !== file1[key]) {
-        acc[`- ${key}`] = file1[key];
-        acc[`+ ${key}`] = file2[key];
+        acc[`  - ${key}`] = file1[key];
+        acc[`  + ${key}`] = file2[key];
       }
       return acc;
     }
     if (_.has(file1, key) && !_.has(file2, key)) {
-      acc[`- ${key}`] = file1[key];
+      acc[`  - ${key}`] = file1[key];
       return acc;
     }
-    acc[`+ ${key}`] = file1[key];
+    acc[`  + ${key}`] = file2[key];
     return acc;
   }, {});
   const entriesOfRO = Object.entries(resultObject);
-  const result = entriesOfRO.reduce((acc, el) => acc.concat(`\n${el[0]}: ${el[1]} `), '');
+  const result = entriesOfRO.reduce((acc, [key, value]) => acc.concat(`\n${key}: ${value} `), '');
   console.log(`{ ${result} \n}`);
 };
 
