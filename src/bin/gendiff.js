07f4b3868d0c0404b9dfd9aeea95cb9d08aae31a
Alex Mitrofanov <alexloves.114@gmail.com>
2021-01-23 14:51:40 +0300

    moved file path finding from gendiff to getdifference

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,8 +6,6 @@ import { createRequire } from 'module';
 import getDifference from '../getdifference.js';
 
 const require = createRequire(import.meta.url);
-const fs = require('fs');
-const path = require('path');
 const packageConfig = require('../../package.json');
 
 const { program } = commander.program;
@@ -18,11 +16,7 @@ program
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> </filepath2>')
   .action((filepath1, filepath2) => {
-    const path1 = path.resolve(filepath1);
-    const path2 = path.resolve(filepath2);
-    const fileContent1 = fs.readFileSync(path1, 'utf8');
-    const fileContent2 = fs.readFileSync(path2, 'utf8');
-    getDifference(fileContent1, fileContent2);
+    console.log(getDifference(filepath1, filepath2));
   });
 
 program.parse(process.argv);
--- src/getdifference.js
+++ src/getdifference.js
@@ -1,8 +1,16 @@
 import _ from 'lodash';
+import { createRequire } from 'module';
 
-const getDifference = (filejson1, filejson2) => {
-  const file1 = JSON.parse(filejson1);
-  const file2 = JSON.parse(filejson2);
+const getDifference = (filepath1, filepath2) => {
+  const require = createRequire(import.meta.url);
+  const fs = require('fs');
+  const path = require('path');
+  const path1 = path.resolve(filepath1);
+  const path2 = path.resolve(filepath2);
+  const fileContent1 = fs.readFileSync(path1, 'utf8');
+  const fileContent2 = fs.readFileSync(path2, 'utf8');
+  const file1 = JSON.parse(fileContent1);
+  const file2 = JSON.parse(fileContent2);
   const keys1 = Object.keys(file1).sort();
   const keys2 = Object.keys(file2).sort();
   const keys = keys2.reduce((acc, key) => {
@@ -30,8 +38,9 @@ const getDifference = (filejson1, filejson2) => {
     return acc;
   }, {});
   const entriesOfRO = Object.entries(resultObject);
-  const result = entriesOfRO.reduce((acc, [key, value]) => acc.concat(`\n${key}: ${value} `), '');
-  console.log(`{ ${result} \n}`);
+  const result = entriesOfRO.reduce((acc, [key, value]) => acc.concat(`\n${key}: ${value}`), '');
+  const res = `{${result}\n}`;
+  return res;
 };
 
 export default getDifference;
