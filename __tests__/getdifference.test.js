d9cbc55e726eb05a58cc7d66bd8921f6f0a3d702
Alex Mitrofanov <alexloves.114@gmail.com>
2021-01-25 19:37:18 +0300

    add test for .yml format

--- __tests__/getdifference.test.js
+++ __tests__/getdifference.test.js
@@ -5,6 +5,8 @@ import getDifference from '../src/getdifference.js';
 const require = createRequire(import.meta.url, '..');
 const filepath1 = require.resolve('../__fixtures__/flatEqual1.json');
 const filepath2 = require.resolve('../__fixtures__/flatEqual2.json');
+const filepath3 = require.resolve('../__fixtures__/file1.yml');
+const filepath4 = require.resolve('../__fixtures__/file2.yml');
 
 const expectedValue = `{
   - follow: false
@@ -15,7 +17,7 @@ const expectedValue = `{
   + verbose: true
 }`;
 
-test('reverse', () => {
+test('getDifference', () => {
   expect(getDifference(filepath1, filepath2)).toEqual(expectedValue);
-  // expect(getDifference).toEqual('');
+  expect(getDifference(filepath3, filepath4)).toEqual(expectedValue);
 });
