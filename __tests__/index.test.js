5d52ee560158b0dea39e32c7257ed393e89db42c
Alex Mitrofanov <alexloves.114@gmail.com>
2021-01-30 15:59:39 +0300

    renamed and fixed file and test getdifference.js to index.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@jest/globals';
 import { createRequire } from 'module';
-import makeRequiredView from '../src/stylish.js';
+import getDiff from '../src/index.js';
 import expectedValueFlat from '../__fixtures__/flatFileResult.js';
 import expectedValueTree from '../__fixtures__/treeFileResult.js';
 
@@ -9,11 +9,11 @@ const dir = '../__fixtures__/';
 const filePath = (fileName) => require.resolve(`${dir}`.concat(`${fileName}`));
 
 test('getDifference for flat files', () => {
-  expect(makeRequiredView(filePath('flatEqual1.json'), filePath('flatEqual2.json'))).toEqual(expectedValueFlat);
-  expect(makeRequiredView(filePath('file1.yml'), filePath('file2.yml'))).toEqual(expectedValueFlat);
+  expect(getDiff(filePath('flatEqual1.json'), filePath('flatEqual2.json'))).toEqual(expectedValueFlat);
+  expect(getDiff(filePath('file1.yml'), filePath('file2.yml'))).toEqual(expectedValueFlat);
 });
 
 test('getDifference for brunched structure', () => {
-  expect(makeRequiredView(filePath('fileTree1.json'), filePath('fileTree2.json'))).toEqual(expectedValueTree);
-  expect(makeRequiredView(filePath('fileTree1.yml'), filePath('fileTree2.yml'))).toEqual(expectedValueTree);
+  expect(getDiff(filePath('fileTree1.json'), filePath('fileTree2.json'))).toEqual(expectedValueTree);
+  expect(getDiff(filePath('fileTree1.yml'), filePath('fileTree2.yml'))).toEqual(expectedValueTree);
 });
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import applyFormatter from './apply-formatter.js';
+import getDiffTree from './getDifTree.js';
+
+const getDiff = (filePath1, filePath2, format = 'stylish') => {
+  const resultFileForFormat = getDiffTree(filePath1, filePath2);
+  const result = applyFormatter(resultFileForFormat, format);
+  return result;
+};
+
+export default getDiff;
