217563fdfa754cde89fba719fc999ed6b10d6ac3
Alex Mitrofanov <alexloves.114@gmail.com>
2021-02-11 20:29:21 +0300

    removed mutation

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -3,10 +3,11 @@ import plain from './plain.js';
 import json from './format-json.js';
 
 const applyFormatter = (file, formatterName) => {
-  const formatters = {};
-  formatters.stylish = stylish;
-  formatters.plain = plain;
-  formatters.json = json;
+  const formatters = {
+    stylish,
+    plain,
+    json,
+  };
   return formatters[formatterName](file);
 };
 
--- src/parse-file.js
+++ src/parse-file.js
@@ -2,17 +2,13 @@ import { createRequire } from 'module';
 import yaml from 'js-yaml';
 
 const parseFile = (fileName) => {
-  let file;
   const require = createRequire(import.meta.url);
   const fs = require('fs');
   const path = require('path');
   const filePath = path.resolve(fileName);
   const fileContent = fs.readFileSync(filePath, 'utf8');
   const format = path.extname(fileName);
-  if (format === 'json') {
-    file = JSON.parse(fileContent);
-  }
-  file = yaml.load(fileContent);
+  const file = (format === 'json') ? JSON.parse(fileContent) : yaml.load(fileContent);
   return file;
 };
 
