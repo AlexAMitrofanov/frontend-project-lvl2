d71f5708629428355de4b76d43b6138f7d7ce270
Alex Mitrofanov <alexloves.114@gmail.com>
2021-02-13 16:30:57 +0300

    removed assignment

--- src/getDifTree.js
+++ src/getDifTree.js
@@ -7,20 +7,21 @@ const getDiffTree = (filePath1, filePath2) => {
   const iter = (file1, file2) => {
     const keys = _.sortBy(_.union(_.keys(file1), _.keys(file2)));
     const resultObject = keys.reduce((acc, key) => {
+      const acc1 = _.cloneDeep(acc);
       if (_.has(file1, key) && _.has(file2, key)) {
         if (file2[key] === file1[key]) {
           Object.assign(acc, { [`  ${key}`]: file2[key] });
-          return Object.assign(acc, { [`  ${key}`]: file2[key] });
+          return Object.assign(acc1, { [`  ${key}`]: file2[key] });
         }
         if ((typeof file1[key] === 'object') && (typeof file2[key] === 'object')) {
-          return Object.assign(acc, { [`  ${key}`]: iter(file1[key], file2[key]) });
+          return Object.assign(acc1, { [`  ${key}`]: iter(file1[key], file2[key]) });
         }
-        return Object.assign(acc, { [`8 ${key}`]: [file1[key], file2[key]] });
+        return Object.assign(acc1, { [`8 ${key}`]: [file1[key], file2[key]] });
       }
       if (!_.has(file1, key) && _.has(file2, key)) {
-        return Object.assign(acc, { [`+ ${key}`]: file2[key] });
+        return Object.assign(acc1, { [`+ ${key}`]: file2[key] });
       }
-      return Object.assign(acc, { [`- ${key}`]: file1[key] });
+      return Object.assign(acc1, { [`- ${key}`]: file1[key] });
     }, {});
     return resultObject;
   };
