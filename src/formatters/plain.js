b891b4819c4f31882d89889c76067fd6f3bc8c4d
Alex Mitrofanov <alexloves.114@gmail.com>
2021-02-23 08:13:56 +0300

    reduced cognitive complexity

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,26 +1,30 @@
 import _ from 'lodash';
 
+const getNewValue = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  return ((typeof value === 'string') ? `'${value}'` : value);
+};
+
 const iter = (file, dir1) => {
   const result = file.reduce((acc, elem) => {
-    const dir = `${dir1}.${elem.key}`;
-    const value = (typeof elem.value === 'string') ? `'${elem.value}'` : elem.value;
+    const dirName = elem.key;
+    const dir = `${dir1}.${dirName}`;
+    const value = getNewValue(elem.value);
     if (elem.status === 'added') {
-      return (!_.isObject(value)) ? `${acc}${dir}' was added with value: ${value}\n`
-        : `${acc}${dir}' was added with value: [complex value]\n`;
+      return `${acc}${dir}' was added with value: ${value}\n`;
     }
     if (elem.status === 'removed') {
       return `${acc}${dir}' was removed\n`;
     }
     if (elem.status === 'changed') {
-      const valueOld = (typeof elem.valueOld === 'string') ? `'${elem.valueOld}'` : elem.valueOld;
-      const valueNew = (typeof elem.valueNew === 'string') ? `'${elem.valueNew}'` : elem.valueNew;
-      if (!_.isObject(valueOld) && !_.isObject(valueNew)) {
-        return `${acc}${dir}' was updated. From ${valueOld} to ${valueNew}\n`;
-      }
-      return (_.isObject(valueOld)) ? `${acc}${dir}' was updated. From [complex value] to ${valueNew}\n`
-        : `${acc}${dir}' was updated. From ${valueOld} to [complex value]\n`;
+      const valueOld = getNewValue(elem.valueOld);
+      const valueNew = getNewValue(elem.valueNew);
+      return `${acc}${dir}' was updated. From ${valueOld} to ${valueNew}\n`;
     }
-    return (_.isObject(value)) ? `${acc}${iter(value, dir)}` : `${acc}`;
+    const valueUnchanged = elem.value;
+    return (_.isObject(valueUnchanged)) ? `${acc}${iter(valueUnchanged, dir)}` : `${acc}`;
   }, '');
   return result;
 };
