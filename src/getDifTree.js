e29a201148ad234694cd173b329e127074b5086c
Alex Mitrofanov <alexloves.114@gmail.com>
2021-02-23 10:47:40 +0300

    reduced cognitive complexity

--- src/getDifTree.js
+++ src/getDifTree.js
@@ -4,21 +4,21 @@ import parseFile from './parse-file.js';
 const iter = (file1, file2) => {
   const keys = _.sortBy(_.union(_.keys(file1), _.keys(file2)));
   const result = keys.reduce((acc, key) => {
-    if (_.has(file1, key) && _.has(file2, key)) {
-      if (file2[key] === file1[key]) {
-        return acc.concat({ key, status: 'unchanged', value: file2[key] });
-      }
-      if (_.isObject(file1[key]) && _.isObject(file2[key])) {
-        return acc.concat({ key, status: 'unchanged', value: iter(file1[key], file2[key]) });
-      }
-      return acc.concat({
-        key, status: 'changed', valueOld: file1[key], valueNew: file2[key],
-      });
-    }
     if (!_.has(file1, key)) {
       return acc.concat({ key, status: 'added', value: file2[key] });
     }
-    return acc.concat({ key, status: 'removed', value: file1[key] });
+    if (!_.has(file2, key)) {
+      return acc.concat({ key, status: 'removed', value: file1[key] });
+    }
+    if (_.isObject(file1[key]) && _.isObject(file2[key])) {
+      return acc.concat({ key, status: 'unchanged', value: iter(file1[key], file2[key]) });
+    }
+    if (file2[key] === file1[key]) {
+      return acc.concat({ key, status: 'unchanged', value: file2[key] });
+    }
+    return acc.concat({
+      key, status: 'changed', valueOld: file1[key], valueNew: file2[key],
+    });
   }, []);
   return result;
 };
