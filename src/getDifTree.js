0e35cc91c7db286ac40c60bb57648d2bb4a604e5
Alex Mitrofanov <alexloves.114@gmail.com>
2021-02-23 11:23:28 +0300

    reduced cognitive complexity

--- src/getDifTree.js
+++ src/getDifTree.js
@@ -3,23 +3,23 @@ import parseFile from './parse-file.js';
 
 const iter = (file1, file2) => {
   const keys = _.sortBy(_.union(_.keys(file1), _.keys(file2)));
-  const result = keys.reduce((acc, key) => {
+  const result = keys.map((key) => {
     if (!_.has(file1, key)) {
-      return acc.concat({ key, status: 'added', value: file2[key] });
+      return { key, status: 'added', value: file2[key] };
     }
     if (!_.has(file2, key)) {
-      return acc.concat({ key, status: 'removed', value: file1[key] });
+      return { key, status: 'removed', value: file1[key] };
     }
     if (_.isObject(file1[key]) && _.isObject(file2[key])) {
-      return acc.concat({ key, status: 'unchanged', value: iter(file1[key], file2[key]) });
+      return { key, status: 'unchanged', value: iter(file1[key], file2[key]) };
     }
     if (file2[key] === file1[key]) {
-      return acc.concat({ key, status: 'unchanged', value: file2[key] });
+      return { key, status: 'unchanged', value: file2[key] };
     }
-    return acc.concat({
+    return {
       key, status: 'changed', valueOld: file1[key], valueNew: file2[key],
-    });
-  }, []);
+    };
+  });
   return result;
 };
 
